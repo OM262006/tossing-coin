34c51c70a13a9c2cec492f97e85b908422993a8a
OM <omyengantiwar26@gmail.com>
2026-03-05 01:54:17 +0530

    Improve user prompts and output messages in toss.py

--- toss.py
+++ toss.py
@@ -1,21 +1,26 @@
-import random
-
+import random 
 score = 0
-
-while True: 
-    user = input("enter t to toss the coin and e to exit:").lower()
-    coin = random.choice(['Heads', 'Tails']).lower()
+while True:
+    user = input("Enter T to toss the coin and E to exit: ").lower()
     if user == "t":
-        print("coin is tossing...")
+        print("Coin is tossing...")
+        coin = random.choice(["heads", "tails"]).lower()
+        
         if coin == "heads":
-            print("its heads congratulations")
+            print("You got heads. Congratulations!")
             score += 1
-            print("number of times u tossed",score)
+            print("Number of times you tossed:", score)
+            print("Your total score:", score)
             break
-        else: 
-            print("its tails better luch next time")
-            score += 1
-            print("number of times u tossed",score)
+        else:
+            print("You got tails. Try once again.")
+            score +=1
+            print("Number of times you tossed:", score)
+        print("Your total score:", score)    
+  
+
     elif user == "e":
-        print("thank you for playing")
+        print("Thank you for playing.")
         break
+    else:
+        print("Invalid input")
