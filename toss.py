13b56b4c9a4deab03aed0afeb4f5b642f3349b12
OM <omyengantiwar26@gmail.com>
2026-03-05 01:05:04 +0530

    Add files via upload

---
+++ toss.py
@@ -0,0 +1,21 @@
+import random
+
+score = 0
+
+while True: 
+    user = input("enter t to toss the coin and e to exit:").lower()
+    coin = random.choice(['Heads', 'Tails']).lower()
+    if user == "t":
+        print("coin is tossing...")
+        if coin == "heads":
+            print("its heads congratulations")
+            score += 1
+            print(score)
+            break
+        else: 
+            print("its tails better luch next time")
+            score += 1
+            print(score)
+    elif user == "e":
+        print("thank you for playing")
+        break
